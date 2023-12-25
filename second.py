414cdd7cc06ff635441ed5d2f999f9989fb4f6e9
SashaChesnokov <chesnokov-an-2017@mail.ru>
2023-12-25 18:45:16 +0300

    Add sort function

--- second.py
+++ second.py
@@ -18,9 +18,33 @@ def read_csv(file_path):
 
     return student_data
 
+def insertion_sort(student_data, key):
+    '''
+    Алгоритм сортировки вставками.
+    
+    Args:
+        student_data (list of dict): Список словарей с данными студентов.
+        key (str): Ключ, по которому будет проводиться сортировка
+    Returns:
+        student_data (list of dict): Отсортированный по возрастанию список словарей с данными студентов.
+    '''
+    
+    for i in range(1, len(student_data)):
+        curent_value = student_data[i]
+        position = i
+
+        while position > 0 and student_data[position-1][key] > curent_value[key]:
+            student_data[position] = student_data[position-1]
+            position -= 1
+        
+        student_data[position] = curent_value
+        
+    return student_data[::-1]
+
+
 def main():
     student_data_from_csv = read_csv('student_new.csv')
-    print(student_data_from_csv)
+    print(insertion_sort(student_data_from_csv, 'grade'))
 
 main()
 
