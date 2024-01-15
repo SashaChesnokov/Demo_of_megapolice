2ebbfb8db73d2114b5b5d52de58f7c1e70b5ec72
SashaChesnokov <chesnokov-an-2017@mail.ru>
2024-01-15 18:38:36 +0300

    Add save function

--- forth.py
+++ forth.py
@@ -1,4 +1,6 @@
 import csv
+from random import choice, shuffle
+from string import ascii_uppercase, ascii_lowercase, digits
 
 def read_csv(file_path):
     '''
@@ -24,5 +26,31 @@ def generate_login(student_data):
         student['login'] = new_login
     return student_data
 
-for i in generate_login(read_csv('student.csv')):
-    print(i)
+def generate_passwords(student_data):
+    for student in student_data:
+        new_password = [choice(ascii_lowercase), choice(ascii_uppercase), choice(digits)] + [choice(ascii_lowercase + ascii_uppercase + digits) for _ in range(5)]
+        shuffle(new_password)
+        new_password = ''.join(new_password)
+        student['password'] = new_password
+    return student_data
+
+def save_to_csv(data, file_name):
+    '''
+    Запись данных в CSV формате в файл.
+    
+    Args:
+    data (list of dict): Список словарей с данными студентов.
+    file_name (str): Имя Файла для сохранения.
+    '''
+
+    with open(file_name, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=data[0].keys())
+        writer.writeheader()
+        writer.writerows(data)
+
+def main():
+    student_data = read_csv('student.csv')
+    student_data = generate_login(generate_passwords(student_data))
+    save_to_csv(student_data, 'forth_task.csv')
+
+main()
